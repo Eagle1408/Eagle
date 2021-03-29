53a178755f1a2261cf40d8707f7fd5821f99fdf5
Eagle <80944411+Eagle1408@users.noreply.github.com>
2021-03-29 09:12:31 +0300

    Update RPMod.py

--- RPMod.py
+++ RPMod.py
@@ -22,7 +22,7 @@ class RPMod(loader.Module):
 
     async def rplistcmd(self, message):
         """Используй: .rplist чтобы посмотреть список рп команд."""
-        await message.edit("<b>• чмок\n• чпок\n• кусь\n• обнять\n• шлеп\n• ударить\n•кинуть камнем\n• выкинуть\n• закопать\n• выкопать\n• запопать\n• послать\n• сосать\n• отлизать\n• выебать\n• пожениться\n• уложить/лечь спать\n• ❤️\n• рука и сердце\n• кинуть на карту\n• купить еду/чипсы\n• купить пиво\n• выпить пиво.</b>")
+        await message.edit("<b>• чмок\n• чпок\n• кусь\n• обнять\n• шлеп\n• ударить\n•кинуть камень\n• выкинуть\n• закопать\n• выкопать\n• запопать\n• послать\n• сосать\n• отлизать\n• выебать\n• пожениться\n• уложить/лечь спать\n• ❤️\n• рука и сердце\n• кинуть на карту\n• купить еду/чипсы\n• купить пиво\n• выпить пиво.</b>")
 
     async def watcher(self, message):
         status = self.db.get("RPMod", "status")
@@ -44,7 +44,7 @@ class RPMod(loader.Module):
                         await message.edit(f"<a href=tg://user?id={me.id}>{me.first_name}</a> шлепнул(-а) <a href=tg://user?id={user.id}>{user.first_name}</a>")
                     if message.text.lower() == "ударить":
                         await message.edit(f"<a href=tg://user?id={me.id}>{me.first_name}</a> ударил(-а) <a href=tg://user?id={user.id}>{user.first_name}</a>")
-                    if message.text.lower() == "кинуть камнем":
+                    if message.text.lower() == "кинуть камень":
                         await message.edit(f"<a href=tg://user?id={me.id}>{me.first_name}</a> кинул(-а) камнем в <a href=tg://user?id={user.id}>{user.first_name}</a>")     
                     if message.text.lower() == "выкинуть":
                         await message.edit(f"<a href=tg://user?id={me.id}>{me.first_name}</a> выкинул(-а) <a href=tg://user?id={user.id}>{user.first_name}</a>")    
